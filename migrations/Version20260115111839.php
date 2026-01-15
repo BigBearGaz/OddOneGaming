0035dd3e08e4927a7a975403e6df7b0aaf4dcf0c
BigBearGaz <toto.rez.21@gmail.com>
2026-01-15 12:20:48 +0100

    test

--- migrations/Version20260115111839.php
+++ migrations/Version20260115111839.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260112144340 extends AbstractMigration
+final class Version20260115111839 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20260112144340 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE takeovers DROP Category');
+        $this->addSql('ALTER TABLE dungeons CHANGE spell1 spell1 LONGTEXT DEFAULT NULL, CHANGE spell2 spell2 LONGTEXT DEFAULT NULL, CHANGE spell3 spell3 LONGTEXT DEFAULT NULL, CHANGE passif passif LONGTEXT DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE takeovers ADD Category VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE dungeons CHANGE spell1 spell1 TEXT DEFAULT NULL, CHANGE spell2 spell2 TEXT DEFAULT NULL, CHANGE spell3 spell3 TEXT DEFAULT NULL, CHANGE passif passif TEXT DEFAULT NULL');
     }
 }
